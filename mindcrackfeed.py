571f9aab4093efa347b3ccb03fb3e8d79efe44f9
Andrew Hughson <andrew@atshughson.me>
2012-08-27 03:38:53 +1200

    Remove handlers for fetching and updating video database

--- mindcrackfeed.py
+++ mindcrackfeed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import database
-import util
 
 import os.path
 import json
@@ -23,9 +22,7 @@ class Application(tornado.web.Application):
         handlers = [
             (r"/", HomeHandler),
             (r"/about", AboutHandler),
-            (r"/videos", VideosHandler),
-            (r"/fetch", FetchVideos),
-            (r"/update", UpdateVideos)
+            (r"/videos", VideosHandler)
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -85,74 +82,6 @@ class VideosHandler(BaseHandler):
 
         self.write(json.dumps(videos, default=dthandler))
         self.finish()
-
-
-class FetchVideos(BaseHandler):
-    def get(self):
-        try:
-            self.get_argument('auth')
-        except:
-            self.write_error(401)
-
-        if self.get_argument('auth') == 'm1ndcr4ckf33d4pp':
-            self.write('[' + str(datetime.utcnow()) + ']')
-            self.write(' fetching new videos ')
-            self.flush()
-
-            for m in self.db.mindcrackers():
-                self.write('fetching ' + m['username'] + '\'s videos ')
-                self.flush()
-
-                for v in util.youtube_feed(m['username'], number_videos=3):
-                    self.db.add_video(**v)
-
-            self.write('rendering template ')
-            self.flush()
-
-            videos = self.db.videos(num_videos=videos_per_page)
-            mindcrackers = self.db.mindcrackers()
-
-            global index
-            index = self.render_string("body.html", videos=videos, mindcrackers=mindcrackers)
-
-            self.write('done fetching ')
-            self.write('[' + str(datetime.utcnow()) + ']')
-
-            self.finish()
-        else:
-            self.write_error(401)
-
-
-class UpdateVideos(BaseHandler):
-    def get(self):
-        try:
-            self.get_argument('auth')
-        except:
-            self.write_error(401)
-
-        if self.get_argument('auth') == 'm1ndcr4ckf33d4pp':
-            self.write('[' + str(datetime.utcnow()) + ']')
-            self.write(' updating videos')
-            self.flush()
-
-            for video in self.db.videos(num_videos=100):
-                try:
-                    video = util.youtube_video_data(video['video_id'])
-                    self.write(' updating ' + video['title'])
-                    self.flush()
-                    self.db.update_video(**video)
-                except:
-                    self.write(' culling ' + video['title'])
-                    self.flush()
-                    self.db.remove_video(video['video_id'])
-
-            self.write(' done updating ')
-            self.write('[' + str(datetime.utcnow()) + ']')
-
-            self.finish()
-
-        else:
-            self.write_error(401)
 
 
 class VideoModule(tornado.web.UIModule):
