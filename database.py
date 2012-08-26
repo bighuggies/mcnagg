3b43cbe1069e385bb8036f358fe345805919f27a
Andrew Hughson <andrew@atshughson.me>
2012-08-27 02:42:46 +1200

    Remove postgres stuff

--- database.py
+++ database.py
@@ -1,81 +1,24 @@
-import psycopg2
-from psycopg2 import extras
+import util
 
-
-def _get_db():
-    # db_url = os.getenv('DATABASE_URL')
-    # conn = None
-
-    # if db_url:
-    #     conn = psycopg2.connect('dbname=ddprh9d8b2m6av host=ec2-23-23-234-207.compute-1.amazonaws.com port=5432 user=mrkjurgyjpbrwt password=bRRcEKwPR7noC5yO0xMot1J7cL sslmode=require')
-    # else:
-    #     conn = psycopg2.connect(database='mindcrackfeed', user='mindcrackfeedapp', password='lol')
-    conn = psycopg2.connect('\
-        dbname=ddprh9d8b2m6av \
-        host=ec2-23-23-234-207.compute-1.amazonaws.com \
-        port=5432 user=mrkjurgyjpbrwt \
-        password=bRRcEKwPR7noC5yO0xMot1J7cL sslmode=require')
-    return conn
-
-
-conn = _get_db()
-cur = conn.cursor(cursor_factory=extras.RealDictCursor)
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE, cur)
-
-
-def add_video(video_id, title, duration, uploader, uploaded, description, thumbnail):
-    cur.execute('SELECT video_id FROM videos WHERE video_id=\'{}\''.format(video_id))
-
-    if cur.fetchone() == None:
-        cur.execute('INSERT INTO videos VALUES (%s, %s, %s, %s, %s, %s, %s);',
-            (video_id, title, duration, uploader.lower(), uploaded, description, thumbnail))
-
-        conn.commit()
-    else:
-        update_video(video_id, title, duration, uploader, uploaded, description, thumbnail)
-
-
-def update_video(video_id, title, duration, uploader, uploaded, description, thumbnail):
-    cur.execute('UPDATE videos SET \
-        title=%s, \
-        duration=%s, \
-        uploaded=%s, \
-        description=%s, \
-        thumbnail=%s \
-        WHERE video_id=%s', (title, duration, uploaded, description, thumbnail, video_id))
-
-    conn.commit()
-
-
-def remove_video(video_id=None):
-    if video_id:
-        cur.execute('DELETE FROM videos WHERE video_id=%s', (video_id, ))
-
-    conn.commit()
-
-
-def add_mindcracker(username, name, url):
-    cur.execute('INSERT INTO mindcrackers VALUES (%s, %s, %s);', (username.lower(), url.lower(), name,))
-    conn.commit()
+mindcrackersdict = [{'username': u'adlingtont', 'url': u'http://www.youtube.com/adlingtont', 'name': u'Adlington'}, {'username': u'bdoubleo100', 'url': u'http://www.youtube.com/bdoubleo100', 'name': u'BdoubleO'}, {'username': u'arkasmc', 'url': u'http://www.youtube.com/arkasmc', 'name': u'Arkas'}, {'username': u'ethoslab', 'url': u'http://www.youtube.com/ethoslab', 'name': u'EthosLab'}, {'username': u'jsano19', 'url': u'http://www.youtube.com/jsano19', 'name': u'Jsano'}, {'username': u'kurtjmac', 'url': u'http://www.youtube.com/kurtjmac', 'name': u'Kurtjmac'}, {'username': u'millbeeful', 'url': u'http://www.youtube.com/millbeeful', 'name': u'MillBee'}, {'username': u'nebris88', 'url': u'http://www.youtube.com/nebris88', 'name': u'Nebris'}, {'username': u'pauseunpause', 'url': u'http://www.youtube.com/pauseunpause', 'name': u'PauseUnpause'}, {'username': u'vintagebeef', 'url': u'http://www.youtube.com/vintagebeef', 'name': u'VintageBeef'}, {'username': u'shreeyamnet', 'url': u'http://www.youtube.com/shreeyamnet', 'name': u'Shreeyam'}, {'username': u'imanderzel', 'url': u'http://www.youtube.com/imanderzel', 'name': u'AnderZEL'}, {'username': u'w92baj', 'url': u'http://www.youtube.com/w92baj', 'name': u'Baj'}, {'username': u'docm77', 'url': u'http://www.youtube.com/docm77', 'name': u'DocM'}, {'username': u'guudeboulderfist', 'url': u'http://www.youtube.com/guudeboulderfist', 'name': u'Guude'}, {'username': u'justd3fy', 'url': u'http://www.youtube.com/justd3fy', 'name': u'JustDefy'}, {'username': u'supermcgamer', 'url': u'http://www.youtube.com/supermcgamer', 'name': u'MCGamer'}, {'username': u'mhykol', 'url': u'http://www.youtube.com/mhykol', 'name': u'Mhykol'}, {'username': u'pakratt13', 'url': u'http://www.youtube.com/pakratt13', 'name': u'Pakratt'}, {'username': u'pyropuncher', 'url': u'http://www.youtube.com/pyropuncher', 'name': u'PyroPuncher'}, {'username': u'thejims', 'url': u'http://www.youtube.com/thejims', 'name': u'TheJims'}, {'username': u'zisteau', 'url': u'http://www.youtube.com/zisteau', 'name': u'Zisteau'}, {'username': u'mindcracknetwork', 'url': u'http://www.youtube.com/mindcracknetwork', 'name': u'MindCrackNetwork'}, {'username': u'generikb', 'url': u'http://www.youtube.com/generikb', 'name': u'GenerikB'}]
 
 
 def mindcrackers():
-    cur.execute('SELECT * FROM mindcrackers')
+    return mindcrackersdict
 
-    return [m for m in cur]
+
+def cached_videos(mindcrackers=tuple([m['username'] for m in mindcrackers()]), num_videos=1, offset=0):
+    videos = []
+
+    for m in mindcrackers:
+        v = util.get_uploads(str(m), num_videos, offset)
+        videos = videos + v
+
+    return sorted(videos, key=lambda v: v['uploaded'])[offset:num_videos]
 
 
 def videos(mindcrackers=tuple([m['username'] for m in mindcrackers()]), num_videos=1, offset=0):
-    if mindcrackers:
-        cur.execute('SELECT * FROM videos, mindcrackers \
-            WHERE videos.uploader IN %s \
-            AND videos.uploader = mindcrackers.username \
-            ORDER BY uploaded DESC LIMIT %s OFFSET %s',
-            (mindcrackers, num_videos, offset))
-
-        return [v for v in cur]
-
-    return []
+    return cached_videos(mindcrackers, num_videos, offset)
 
 
 def main():
